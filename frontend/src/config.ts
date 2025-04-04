870543caa9cbdb902bc066e8220b41075ad8a2a6
Mashkin E.V. <mev.86@list.ru>
2025-04-05 01:57:13 +0300

    config.ts-изменен IP сервера, README.md обновлен

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000';
-// const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'http://95.163.223.37:8000'; // на сервере
+// const API_BASE_URL = 'http://localhost:8000'; // на локалке
 
 export default API_BASE_URL;
